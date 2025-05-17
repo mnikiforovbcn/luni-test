2ea4be9694581b38725af028cac2aab9fdcea0f7
Mikhail Nikiforov <m.nikiforov@gmail.com>
2025-05-17 22:04:40 +0200

    reorganize folders

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, send_from_directory 
+import os 
   
-app = Flask(__name__, template_folder='.')  
+app = Flask(__name__, template_folder='.', static_folder='frontend/static')  
   
 @app.route("/")  
 def web():  
     return render_template('frontend/index.html')  
-  
+
+
+@app.route("/scripts/<path:filename>")
+def serve_scripts(filename):
+    return send_from_directory(os.path.join("frontend", "scripts"), filename)
+
 if __name__ == "__main__":  
     app.run(debug=True, host="0.0.0.0", port='80') 
